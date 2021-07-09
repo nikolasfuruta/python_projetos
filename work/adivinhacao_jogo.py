7c0da3aff7efdb781f58d9eae7ac8b0cef5ad526
nikolasfuruta <nikolasfuruta@gmail.com>
2021-07-09 12:38:50 -0300

    adivinhacao-commit01

--- work/adivinhacao_jogo.py
+++ work/adivinhacao_jogo.py
@@ -17,17 +17,17 @@ class Adivinhacao:
     def _iniciar(self):
 
         print('''
-        *********************************
-        Bem vindo ao jogo de adivinhação!
-        Tente descobrir qual o número que
-        o computador está pensando!!
-        *********************************
+*********************************
+Bem vindo ao jogo de adivinhação!
+Tente descobrir qual o número que
+o computador está pensando!!
+*********************************
         
-        Escolha um nível de dificuldade:
-        [0] - Sair do jogo
-        [1] - Fácil
-        [2] - Médio
-        [3] - Difícil
+Escolha um nível de dificuldade:
+[0] - Sair do jogo
+[1] - Fácil
+[2] - Médio
+[3] - Difícil
         ''')
         escolha = int(input("Digite sua escolha: "))
         while escolha not in [0,1,2,3]:
@@ -50,7 +50,6 @@ class Adivinhacao:
 
 
     def _get_guess(self,dificuldade):
-        print('Digite zero para sair do jogo\n')
         if dificuldade == 1:
             guess = int(input("Digite o seu palpite: "))
             while guess not in range(0, 20):
@@ -83,6 +82,7 @@ class Adivinhacao:
                 if guess < num:
                     print("Ainda não, o número que você escolheu é menor do que o número que pensei.\n")
                     ponto -= 1
+                    continue
                 elif guess > num:
                     print("O número que você escolheu é maior do que o número que pensei.\n")
                     ponto -= 1
@@ -100,9 +100,8 @@ class Adivinhacao:
         while cont not in 'sn':
             cont = str(input("Deseja jogar novamente? [s/n]: "))
         if cont == 's':
-            self._iniciar()
+            return Adivinhacao()
         else:
-            print('Saindo do jogo de adivinhação.')
             self._voltar_intro()
 
 if __name__ == "__main__":
