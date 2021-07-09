83b4d4ff5fdc94ccc08ad0d258f96b3e7f37e155
nikolasfuruta <nikolasfuruta@gmail.com>
2021-07-09 16:38:23 -0300

    adivinhação - commit 04

--- work/adivinhacao_jogo.py
+++ work/adivinhacao_jogo.py
@@ -5,8 +5,9 @@ class Adivinhacao:
 
     def __init__(self):
         self._dificuldade = self._iniciar()
-        self._num = self._get_number(self._dificuldade)
-        self._play(self._dificuldade,self._num)
+        self._num = self._get_number()
+        self._ponto = 10
+        self._play()
 
     def _voltar_intro(self):
         print('Saindo do jogo de adivinhação.')
@@ -37,69 +38,70 @@ Escolha um nível de dificuldade:
         return escolha
 
 
-    def _get_number(self,dificuldade):
-        if dificuldade == 1:
+    def _get_number(self):
+        if self._dificuldade == 1:
             print("Estou pensando em um número inteiro entre 1 ao 20.")
             return randint(1, 21)
-        elif dificuldade == 2:
+        elif self._dificuldade == 2:
             print("Estou pensando em um número inteiro entre 1 ao 50.")
             return randint(1, 51)
-        elif dificuldade == 3:
+        elif self._dificuldade == 3:
             print("Estou pensando em um número inteiro entre 1 ao 100.")
             return randint(1, 101)
 
 
-    def _get_guess(self,dificuldade):
-        if dificuldade == 1:
-            guess = int(input("Digite o seu palpite: "))
-            while guess not in range(0, 20):
+    def _get_guess(self):
+        if self._dificuldade == 1:
+            palpite = int(input("Digite o seu palpite: "))
+            while palpite not in range(0, 20):
                 print("Você está pensando em um número fora dos limites das regras.")
-                guess = int(input("Digite o seu palpite: "))
-            return guess
+                palpite = int(input("Digite o seu palpite: "))
+            return palpite
 
-        elif dificuldade == 2:
-            guess = int(input("Digite o seu palpite: "))
-            while guess not in range(0, 50):
+        elif self._dificuldade == 2:
+            palpite = int(input("Digite o seu palpite: "))
+            while palpite not in range(0, 50):
                 print("Você está pensando em um número fora dos limites das regras.")
-                guess = int(input("Digite o seu palpite: "))
-            return guess
+                palpite = int(input("Digite o seu palpite: "))
+            return palpite
 
-        elif dificuldade == 3:
-            guess = int(input("Digite o seu palpite: "))
-            while guess not in range(0, 100):
-                print("Você está pensando em um número fora dos limites das regras.")
-                guess = int(input("Digite o seu palpite: "))
-            return guess
+        elif self._dificuldade == 3:
+            palpite = int(input("Digite o seu palpite: "))
+            while palpite not in range(0, 100):
+                palpite = int(input("Digite o seu palpite: "))
+            return palpite
 
 
-    def _play(self,dificuldade,num):
-        ponto = 10
-        while ponto > 0:
-            guess = self._get_guess(dificuldade)
-            if guess == 0: break
-
+    def _avaliar(self,guess):
+        if guess == 0:
+            return False
+        else:
+            if guess < self._num:
+                print("Ainda não, o número que você escolheu é menor do que o número que pensei.\n")
+                self._ponto -= 1
+                return True
+            elif guess > self._num:
+                print("O número que você escolheu é maior do que o número que pensei.\n")
+                self._ponto -= 1
+                return True
             else:
-                if guess < num:
-                    print("Ainda não, o número que você escolheu é menor do que o número que pensei.\n")
-                    ponto -= 1
-                    continue
-                elif guess > num:
-                    print("O número que você escolheu é maior do que o número que pensei.\n")
-                    ponto -= 1
-                    continue
-                else:
-                    print(f"Parabéns, você acertou!!! O número é o {guess}")
-                    print(f"Sua pontuação foi de {ponto}")
-                break
+                print(f"Parabéns, você acertou!!! O número é o {guess}")
+                print(f"Sua pontuação foi de {self._ponto}")
+                return False
 
-        if ponto == 0:
+
+    def _play(self):
+        cont = True
+        while cont:
+            guess = self._get_guess()
+            cont = self._avaliar(guess)
+        if self._ponto == 0:
             print(f'Você zerou a sua potuação.')
             print('GAMEOVER')
-
-        cont = str(input("Deseja jogar novamente? [s/n]: "))
-        while cont not in 'sn':
-            cont = str(input("Deseja jogar novamente? [s/n]: "))
-        if cont == 's':
+        jogar = str(input("Deseja jogar novamente? [s/n]: "))
+        while jogar not in 'sn':
+            jogar = str(input("Deseja jogar novamente? [s/n]: "))
+        if jogar == 's':
             return Adivinhacao()
         else:
             self._voltar_intro()
