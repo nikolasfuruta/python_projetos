65f334d67c4b2e017d3e04b026ad1129a375ec52
nikolasfuruta <nikolasfuruta@gmail.com>
2021-07-11 13:25:03 -0300

    geral - commit 09

--- work/intro_jogo.py
+++ work/intro_jogo.py
@@ -1,4 +1,6 @@
-from work.adivinhacao_jogo import Adivinhacao
+from adivinhacao_jogo import Adivinhacao
+from forca_jogo import Forca
+from par_impar import ParImpar
 
 class Intro:
 
@@ -7,28 +9,28 @@ class Intro:
 
     def _iniciar(self):
         print('''
-        ********************************
-        ******** Escolha o jogo ********
-        ********************************
+********************************
+******** Escolha o jogo ********
+********************************
         ''')
 
-        cont = 's'
-        while cont == 's':
+        while True:
             print('''
-            Escolha qual jogo você quer jogar: 
-            Adivinhação - [1]   Forca - [2]
+Escolha qual jogo você quer jogar: 
+Sair - [0]  Adivinhação - [1]   Forca - [2]  Par ou Impar - [3]
             ''')
-            jogar = int(input("Digite a sua opção: \n"))
+            jogar = int(input("Digite a sua opção: "))
+            while jogar not in [0,1,2,3]:
+                print('Escolha somente as opções disponíveis.')
+                jogar = int(input("Digite a sua opção: "))
 
-            if jogar == 1:
-                return Adivinhacao()
-            elif jogar ==2:
-                pass
-            cont = str(input("Deseja jogar novamente? [s/n]: "))
-            while cont not in 'sn':
-                cont = str(input("Deseja jogar novamente? [s/n]: "))
-            if cont == 'n':
+            if jogar == 0:
                 print("Obrigado por jogar!")
+                break
+            elif jogar == 1: return Adivinhacao()
+            elif jogar == 2: return Forca()
+            elif jogar == 3: return ParImpar()
+
 
 if __name__ == "__main__":
     i = Intro()
